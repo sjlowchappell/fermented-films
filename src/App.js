ebaf2d338f89ca27704e8b891b2bd8cb2bdb1201
Sam Low-Chappell <sam@hackeryou.com>
2019-09-24 16:18:01 -0400

    added uuid package and adjusted form such that state is held in App parent

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header';
 import Form from './components/form';
 import Footer from './components/footer';
 
-function App() {
-	const ingredients = ['lime', 'cream'];
-	return (
-		<div className="App">
-			<Header />
-			<main>
-				<div className="wrapper">
-					<p>
-						With Fermented Films, pick an ingredient theme for the evening. Specify any dietary restrictions
-						and we'll curate a meal, drink, and movie combination around your selections. Don't like one of
-						the available options? Click "SHake it up!" and we'll find another that suits your needs.
-					</p>
+const ingredients = ['lime', 'cream'];
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			ingredient: 'lime',
+			restrictions: 'No Restrictions',
+		};
+	}
 
-					<Form ingredients={ingredients} />
-				</div>
-			</main>
-			<Footer />
-		</div>
-	);
+	handleFormSubmit = e => {
+		e.preventDefault();
+		console.log(this.state);
+	};
+	handleFormChange = e => {
+		const key = e.target.id;
+		const value = e.target.value;
+		this.setState({
+			[key]: value,
+		});
+	};
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<main>
+					<div className="wrapper">
+						<p>
+							With Fermented Films, pick an ingredient theme for the evening. Specify any dietary
+							restrictions and we'll curate a meal, drink, and movie combination around your selections.
+							Don't like one of the available options? Click "Shake it up!" and we'll find another that
+							suits your needs.
+						</p>
+						<Form
+							ingredient={this.state.ingredient}
+							ingredients={ingredients}
+							handleFormSubmit={this.handleFormSubmit}
+							handleFormChange={this.handleFormChange}
+						/>
+					</div>
+				</main>
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/form.js
+++ src/components/form.js
@@ -1,48 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import uuidv4 from 'uuid/v4';
 
-class Form extends Component {
-	constructor({ ingredients }) {
-		super();
-		this.state = {
-			ingredients: ingredients,
-			ingredient: 'lime',
-			restrictions: 'No Restrictions',
-		};
-	}
+function Form({ ingredient, ingredients, handleFormSubmit, handleFormChange }) {
+	return (
+		<form>
+			<label htmlFor="ingredient">Pick an Ingredient Theme</label>
+			<select onChange={handleFormChange} name="ingredient" id="ingredient" value={ingredient}>
+				{ingredients.map(ingredient => {
+					return (
+						<option value={ingredient} key={uuidv4()}>
+							{ingredient}
+						</option>
+					);
+				})}
+			</select>
 
-	handleChange = e => {
-		const key = e.target.id;
-		const value = e.target.value;
-		this.setState({
-			[key]: value,
-		});
-	};
+			<label htmlFor="restrictions">Are you vegetarian or vegan?</label>
+			<select onChange={handleFormChange} name="restrictions" id="restrictions">
+				<option value="No Restrictions">No Restrictions</option>
+				<option value="Vegetarian">Vegetarian</option>
+				<option value="Vegan">Vegan</option>
+			</select>
 
-	handleSubmit = e => {
-		e.preventDefault();
-	};
-
-	render() {
-		return (
-			<form>
-				<label htmlFor="ingredient">Pick an Ingredient Theme</label>
-				<select onChange={this.handleChange} name="ingredient" id="ingredient" value={this.state.ingredient}>
-					{this.state.ingredients.map(ingredient => {
-						return <option value={ingredient}>{ingredient}</option>;
-					})}
-				</select>
-
-				<label htmlFor="restrictions">Are you vegetarian or vegan?</label>
-				<select onChange={this.handleChange} name="restrictions" id="restrictions">
-					<option value="No Restrictions">No Restrictions</option>
-					<option value="Vegetarian">Vegetarian</option>
-					<option value="Vegan">Vegan</option>
-				</select>
-
-				<button onClick={this.handleSubmit}>Submit</button>
-			</form>
-		);
-	}
+			<button onClick={handleFormSubmit}>Submit</button>
+		</form>
+	);
 }
 
 export default Form;
