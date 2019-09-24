04766b0f55d1142d65e1654a119841d2b08baeee
Sam Low-Chappell <sam@hackeryou.com>
2019-09-24 13:19:36 -0400

    added components for header, footer, form, and recommendations

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<footer>
+			<div className="wrapper">© {new Date().getFullYear()} built by Sam Low-Chappell</div>
+		</footer>
+	);
+}
+
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+	return (
+		<header>
+			<div className="wrapper">
+				<h1>Fermented Films</h1>
+				<p>One stop shop for a curated evening based on one ingredient</p>
+			</div>
+		</header>
+	);
+}
+
+export default Header;
