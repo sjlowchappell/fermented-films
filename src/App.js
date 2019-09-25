70549a9ea72e311cc265a09f64dad5aa0c0b2377
Sam Low-Chappell <sam@hackeryou.com>
2019-09-25 17:17:30 -0400

    Added functionality such that state saves list of meal, drink, and movie options, plus the current meal, drink, and movie

--- src/App.js
+++ src/App.js
@@ -21,30 +21,72 @@ class App extends Component {
 		});
 	};
 
+	getDataForList = list => {
+		return list.map(item => {
+			return this.fetchData(item.url, item.params);
+		});
+	};
+
+	chooseNewOption = (list, num) => {
+		const newIndex = Math.floor(Math.random() * num);
+		return list[newIndex];
+	};
+
 	handleFormSubmit = async e => {
 		e.preventDefault();
 		const drinkParams = { i: this.state.ingredient };
 		const movieParams = {
 			api_key: '78bc17b4e102a33a55c252cd4873cbe7',
-			langue: 'en-US',
+			language: 'en-US',
 			query: this.state.ingredient,
 			page: 1,
 			include_adult: false,
 		};
-		const requests = [
-			{ url: 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?', params: drinkParams },
-			{ url: 'https://www.themealdb.com/api/json/v1/1/filter.php?', params: drinkParams },
+		const initialDataRequests = [
+			{
+				url: 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?',
+				params: drinkParams,
+			},
+			{
+				url: 'https://www.themealdb.com/api/json/v1/1/filter.php?',
+				params: drinkParams,
+			},
 			{ url: 'https://api.themoviedb.org/3/search/movie?', params: movieParams },
 		];
-		const requestList = requests.map(request => {
-			return this.fetchData(request.url, request.params);
-		});
-		const info = await Promise.all(requestList);
-		console.log(info);
+		const requestList = this.getDataForList(initialDataRequests);
+		const initialInfo = await Promise.all(requestList);
+
+		const drinkList = initialInfo[0].data.drinks;
+		const mealList = initialInfo[1].data.meals;
+		const movieList = initialInfo[2].data.results;
+		const newDrink = this.chooseNewOption(drinkList, drinkList.length);
+		const newMeal = this.chooseNewOption(mealList, mealList.length);
+		const newMovie = this.chooseNewOption(movieList, movieList.length);
+		console.log(newMovie);
+		const secondMovieParams = {
+			api_key: '78bc17b4e102a33a55c252cd4873cbe7',
+			language: 'en-US',
+		};
+
+		const secondDataRequests = [
+			{ url: 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?', params: { i: newDrink.idDrink } },
+			{ url: 'https://www.themealdb.com/api/json/v1/1/lookup.php?', params: { i: newMeal.idMeal } },
+			{ url: `https://api.themoviedb.org/3/movie/${newMovie.id}`, params: secondMovieParams },
+		];
+
+		const secondRequestList = this.getDataForList(secondDataRequests);
+		const secondInfo = await Promise.all(secondRequestList);
+
+		const currentDrink = secondInfo[0].data.drinks[0];
+		const currentMeal = secondInfo[1].data.meals[0];
+		const currentMovie = secondInfo[2].data;
 		this.setState({
-			drinkOptions: info[0].data.drinks,
-			mealOptions: info[1].data.meals,
-			movieOptions: info[2].data.results,
+			drinkOptions: drinkList,
+			mealOptions: mealList,
+			movieOptions: movieList,
+			currentDrink: currentDrink,
+			currentMeal: currentMeal,
+			currentMovie: currentMovie,
 		});
 	};
 	handleFormChange = e => {
