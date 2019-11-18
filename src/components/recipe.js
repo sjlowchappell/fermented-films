85f0a0041a8c5797e38d49b43938c57744fb8338
Sam Low-Chappell <sam@hackeryou.com>
2019-11-18 09:59:42 +0100

    added ability to collapse and expand ingredients based on number of them

--- src/components/recipe.js
+++ src/components/recipe.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import uuidv4 from 'uuid/v4';
+import plusIcon from '../assets/plus.svg';
+import minusIcon from '../assets/minus.svg';
 
 function Recipe({ name, image, ingredientList, instructions, onClick, value }) {
+	const expandDescription = e => {
+		e.stopPropagation();
+		const description = e.target.parentElement.previousElementSibling;
+		const icon = e.target;
+		console.log(description);
+		console.log(icon);
+		if (description.classList.contains('descriptionClosed')) {
+			description.classList.toggle('descriptionClosed');
+			icon.classList.toggle('closedIcon');
+			icon.nextElementSibling.classList.toggle('closedIcon');
+		} else {
+			description.classList.toggle('descriptionClosed');
+			icon.classList.toggle('closedIcon');
+			icon.previousElementSibling.classList.toggle('closedIcon');
+		}
+	};
 	return (
 		<div className="wrapper">
 			<h2>{name}</h2>
@@ -10,9 +28,9 @@ function Recipe({ name, image, ingredientList, instructions, onClick, value }) {
 				<div className="recipeImage">
 					<img src={image} alt={image} />
 				</div>
-				<div>
+				<div className="ingredientsContainer">
 					<p>Ingredients:</p>
-					<ul>
+					<ul className="descriptionClosed">
 						{ingredientList.map(ingredient => {
 							return (
 								<li key={uuidv4()}>
@@ -21,6 +39,13 @@ function Recipe({ name, image, ingredientList, instructions, onClick, value }) {
 							);
 						})}
 					</ul>
+					{/* if the description is too long, add a button to expand it */}
+					{ingredientList.length > 10 ? (
+						<button className="expandButton" onClick={expandDescription}>
+							<img className="svgIcon" src={plusIcon} alt="Expand Description" />
+							<img className="closedIcon svgIcon" src={minusIcon} alt="Collapse Description" />
+						</button>
+					) : null}
 				</div>
 			</div>
 			<p className="specialWord">Instructions:</p>
